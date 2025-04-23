15ea2cfcbe26137d0def55947a7e69e6d1f62e35
riyakannapareddy <riyak4321@gmail.com>
2025-04-23 18:18:52 -0500

    updated endpoint to allow base 64 images

--- server/main.py
+++ server/main.py
@@ -2,17 +2,12 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from inference_sdk import InferenceHTTPClient
 import os
+import requests
 from dotenv import load_dotenv
 
-#global variables
+#reusable variables
 load_dotenv()
 api_key = os.getenv("ROBOFLOW_API_KEY")
-
-CLIENT = InferenceHTTPClient(
-    api_url = "https://classify.roboflow.com",
-    api_key = api_key
-)
-
 model_id = "banana-classification-eprkr/2"
 
 app = Flask(__name__)
@@ -22,12 +17,22 @@ cors = CORS(app, origins="*")
 def users():
     return jsonify({"users": ["Alice", "Bob", "Charlie"]})
 
+#route which takes in an image and returns the classification
 @app.route('/classify', methods=['POST'])
 def classify():
-    image = "placeholder"
-    #TODO:get image from request on front end
-    classification = CLIENT.infer(image, model_id)
-    return jsonify(classification)
+    data = request.json
+    img = data.get("image")
+    if not img:
+        return jsonify({"error" : "No image provided"}), 400
+    
+    roboflow_url = f"https://classify.roboflow.com/{model_id}?api_key={api_key}"
+
+    response = requests.post(
+        roboflow_url,
+        json={"image":base64_image.split(","[1] if "," in base64_image else base64_image)},
+        headers={"Content-Type": "application/json"}
+    )
+    return jsonify(response.json())
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
